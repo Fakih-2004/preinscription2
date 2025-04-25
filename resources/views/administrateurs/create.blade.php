5bf9ee1d5f8ba86455ca4441abd8ddcb42056cae
Noura <nourayazami2004@email.com>
2025-04-25 11:48:28 +0100

    doennnnn

--- resources/views/administrateurs/create.blade.php
+++ resources/views/administrateurs/create.blade.php
@@ -10,16 +10,7 @@
             <form action="{{ route('administrateurs.store') }}" method="POST">
                 @csrf
                 
-                <div class="mb-3">
-                    <label for="formation_id" class="form-label">Type de formation</label>
-                    <select name="formation_id" id="formation_id" class="form-select" required>
-                        <option value="">-- Choisir --</option>
-                        @foreach ($formations as $formation)
-                            <option value="{{ $formation->id }}">{{ ucfirst($formation->type_formation) }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                
+               
                 <div class="mb-3">
                     <label for="nom" class="form-label">Nom</label>
                     <input type="text" name="nom" id="nom" class="form-control" placeholder="Entrez le nom" required>
